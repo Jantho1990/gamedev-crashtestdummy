126132a69100bf10e4c419575c094aa4c3ec8026
Joshua Anthony <jantho1990@gmail.com>
2018-09-22 19:29:31 -0500

    using polar coordinates

--- src/entities/CrashTestDummy.js
+++ src/entities/CrashTestDummy.js
@@ -11,15 +11,20 @@ class CrashTestDummy extends TileSprite {
     super(texture, 48, 48)
     this.pivot = { x: 24, y: 24 }
     this.frame.x = math.rand(4)
+
+    this.dir = math.randf(Math.PI * 2)
+    this.speed = math.rand(50, 150)
+
     this.vel = new Vec()
     this.acc = new Vec()
+
     this.bounds = bounds
   }
 
   update(dt) {
-    const { pos, vel, bounds, w, h } = this
+    const { pos, dir, speed, bounds, w, h } = this
 
-    if (math.randOneIn(130)) {
+    /* if (math.randOneIn(130)) {
       physics.applyImpulse(
         this,
         {
@@ -28,24 +33,30 @@ class CrashTestDummy extends TileSprite {
         },
         dt
       )
-    }
+    } */
 
-    physics.applyFriction(this, 100);
+    this.rotation = this.dir + Math.PI / 4
+    pos.add({
+      x: Math.cos(dir) * speed * dt,
+      y: Math.sin(dir) * speed * dt
+    })
+
+    // physics.applyFriction(this, 100);
     
-    const gravity = { x: 0, y: 500 }
-    physics.applyForce(this, gravity)
+    // const gravity = { x: 0, y: 500 }
+    // physics.applyForce(this, gravity)
     
     physics.integratePos(this, dt);
 
     // Bounce off the walls
     if (pos.x < 0 || pos.x > bounds.w - w) {
-      vel.x *= -1
+      // vel.x *= -1
+      this.dir = -this.dir + Math.PI
     }
     if (pos.y < 0 || pos.y > bounds.h - h) {
-      vel.y *= -1
+      // vel.y *= -1
+      this.dir = -this.dir
     }
-
-    this.rotation += vel.x * 0.05 * dt
   }
 }
 
